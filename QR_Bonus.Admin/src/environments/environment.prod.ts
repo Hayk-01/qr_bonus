a39787f7658a92baac230dbcb33d6049e4ff73c6
Hayk <78102270+Hayk-01@users.noreply.github.com>
2026-02-13 17:19:10 +0400

    Update production environment URLs

--- QR_Bonus.Admin/src/environments/environment.prod.ts
+++ QR_Bonus.Admin/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: "https://qrbonusadminapi.martinyerevan.am/api/",
-  mediaUrl: "https://qrbonusadminapi.martinyerevan.am/media",
-  cdn: "https://cdnqrbonus.martinyerevan.am/statictexts"
+  url: "https://vboxuserapi.innline.am/api/",
+  mediaUrl: "https://vboxuserapi.innline.am/media",
+  cdn: "https://vboxuserapi.innline.am/statictexts"
 };
