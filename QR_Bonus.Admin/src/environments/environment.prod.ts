552e4d3bdd1ca1db576d56fc717e687155160a71
Hayk <78102270+Hayk-01@users.noreply.github.com>
2026-02-13 18:36:26 +0400

    Update CDN URL in production environment

--- QR_Bonus.Admin/src/environments/environment.prod.ts
+++ QR_Bonus.Admin/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   url: "https://vboxuserapi.innline.am/api/",
   mediaUrl: "https://vboxuserapi.innline.am/media",
-  cdn: "https://vboxuserapi.innline.am/statictexts"
+  cdn: "https://vboxuserweb.innline.am/statictexts"
 };
